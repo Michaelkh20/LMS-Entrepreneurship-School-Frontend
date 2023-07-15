4f626a7b098ee82fade59f168b43f6794811eb0c
kadjit228 <kadjit228@mail.ru>
2023-07-16 00:45:19 +0500

    несколько страниц из админа

---
+++ frontend/src/app/admin/accounts/[id]/page.tsx
@@ -0,0 +1,38 @@
+import HeaderAdminBig from "@/components/headers/header-admin-big/header";
+import style from './page.module.css';
+
+export default function Page({ params }: { params: { id: string } }) {
+    return (
+        <div>
+            <HeaderAdminBig />
+            <h1 className={style.title}> Профиль </h1>
+            <div>ID IS: {params.id}</div>
+            <div className={style.row}>
+                <div className={style.left}>
+                    <h1 className={style.name}>Имя Студента из API</h1>
+                    <h1 className={style.role}> РОЛЬ из API </h1>
+                    <button className={style.buttonRedux}> Редактировать </button>
+                </div>
+                <div className={style.balance}>
+                    Баланс: 777
+                </div>
+            </div>
+            <div className={style.line}>
+                Email <h1 className={style.role}> mail </h1>
+            </div>
+            <div className={style.line}>
+                Email <h1 className={style.role}> mail </h1>
+            </div>
+            <div className={style.line}>
+                Email <h1 className={style.role}> mail </h1>
+            </div>
+            <div className={style.line}>
+                Email <h1 className={style.role}> mail </h1>
+            </div>
+            <div className={style.line}>
+                Email <h1 className={style.role}> mail </h1>
+            </div>
+        </div>
+
+    )
+}
---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styles from './admin.module.css';
+import HeaderAdmin from '@/components/headers/header-admin/header';
+
+const AdminPage: React.FC = () => {
+
+    const rectangles = [
+        { text: 'Аккаунты', link: '/accounts' },
+        { text: 'Команды', link: '/teams' },
+        { text: 'Оценки', link: '/grades' },
+        { text: 'Заявки', link: '/applications' },
+        { text: 'Транзакции', link: '/transactions' },
+        { text: 'Лоты', link: '/lots' },
+        { text: 'Темы', link: '/topics' },
+        { text: 'Задания', link: '/tasks' },
+        { text: 'Посещаемость', link: '/attendance' },
+        { text: 'Сданные файлы', link: '/submitted-files' },
+        { text: 'Редактировать формулу оценивания', link: '/edit-grading-formula' },
+    ];
+
+    return (
+        <>
+            <HeaderAdmin />
+            <div className={styles.buttonRow}>
+                <div></div>
+                <button className={styles.button}>Создать рассылку</button>
+            </div>
+            <div className={styles.row}>
+                {rectangles.slice(0,4).map((rectangle, index) => (
+                    <div
+                        key={index}
+                        className={styles.rectangle}
+                    >
+                        <p>{rectangle.text}</p>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.row}>
+                {rectangles.slice(4,7).map((rectangle, index) => (
+                    <div
+                        key={index}
+                        className={styles.rectangle}
+                    >
+                        <p>{rectangle.text}</p>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.row}>
+                {rectangles.slice(7,10).map((rectangle, index) => (
+                    <div
+                        key={index}
+                        className={styles.rectangle}
+                    >
+                        <p>{rectangle.text}</p>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+};
+export default AdminPage;
