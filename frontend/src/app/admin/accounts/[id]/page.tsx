45ff180d1670e698efab0b75583456072cb79b67
Nikita <n.d.2002z@gmail.com>
2023-07-18 14:33:01 +0300

    (dev) some fixes in /admin/accounts

--- frontend/src/app/admin/accounts/[id]/page.tsx
+++ frontend/src/app/admin/accounts/[id]/page.tsx
@@ -1,17 +1,15 @@
-import HeaderAdminBig from "@/components/headers/header-admin-big/header";
 import style from './page.module.css';
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({params}: { params: { id: string } }) {
     return (
         <div>
-            <HeaderAdminBig />
             <h1 className={style.title}> Профиль </h1>
             <div>ID IS: {params.id}</div>
             <div className={style.row}>
                 <div className={style.left}>
                     <h1 className={style.name}>Имя Студента из API</h1>
                     <h1 className={style.role}> РОЛЬ из API </h1>
-                    <button className={style.buttonRedux}> Редактировать </button>
+                    <button className={style.buttonRedux}> Редактировать</button>
                 </div>
                 <div className={style.balance}>
                     Баланс: 777
