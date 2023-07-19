a755cd868a5bc4e455d402a413a6ab05bbacae2c
Nikita <n.d.2002z@gmail.com>
2023-07-19 15:20:16 +0300

    (dev) fixed render flickers

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,26 +1,30 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Mulish } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Mulish} from 'next/font/google'
+
+import StyledComponentsRegistry from '../lib/AntdRegistry';
 
 const mulish = Mulish({
-  weight: ["300", "500", "700"],
-  style: "normal",
-  subsets: ["cyrillic", "latin"]
+    weight: ["300", "500", "700"],
+    style: "normal",
+    subsets: ["cyrillic", "latin"]
 })
 
 export const metadata: Metadata = {
-  title: 'Entrepreneurship School',
-  description: '',
+    title: 'Entrepreneurship School',
+    description: '',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="ru">
-      <body className={mulish.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="ru">
+        <body className={mulish.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </body>
+        </html>
+    )
 }
