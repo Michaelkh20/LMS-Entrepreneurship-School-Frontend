070d9118008cba23f6d77e6000b20146b0e3862a
3uo2khte66h <andrey_2468@mail.ru>
2023-07-28 21:58:50 +0300

    fix lot card + shop page

---
+++ frontend/src/app/student/shop/page.tsx
@@ -0,0 +1,102 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Mulish, Jura } from 'next/font/google'
+import Link from 'next/link'
+import HeaderLernerBig from "@/components/headers/header-lerner-big/header"
+import TradeLotCard from "@/components/LotCard/lot-card"
+
+const mulish = Mulish({
+  subsets: ['latin']
+})
+
+const jura = Jura({
+  subsets: ['latin']
+})
+
+export default function Home() {
+  return (
+    <main className={mulish.className}>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <HeaderLernerBig/>
+          <div className={jura.className}>
+              <h1 className={styles.page_label}>МАГАЗИН</h1>
+          </div>
+
+          <div className={styles.lots_container}>
+
+            <TradeLotCard
+                number={12345}
+                description="Торговый лот описания"
+                performer="Имя исполнителя"
+                price={200}
+                conditions="Условия торгового лота"
+            />
+            <TradeLotCard
+                number={12345}
+                description="Торговый лот описания"
+                performer="Имя исполнителя"
+                price={200}
+                conditions="Условия торгового лота"
+            />
+            <TradeLotCard
+                number={12345}
+                description="Торговый лот описания"
+                performer="Имя исполнителя"
+                price={200}
+                conditions="Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота "
+            />
+            <TradeLotCard
+                number={12345}
+                description="Торговый лот описания"
+                performer="Имя исполнителя"
+                price={200}
+                conditions="Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота Условия торгового лота "
+            />
+
+          </div>
+
+          <div className={styles.create_lot}>
+            <div className={jura.className}>
+              <h1>Предложить свой лот</h1>
+            </div>
+            <div className={styles.create_lot_data}>
+              <div className={styles.left_create_lot_data}>
+                <div className={styles.label_and_input_box}>
+                  <p>Название лота*</p>
+                  <input></input>
+                </div>
+                <div className={styles.label_and_input_box}>
+                  <p>Описание*</p>
+                  <input></input>
+                </div>
+              </div>
+              <div className={styles.right_create_lot_data}>
+                <div className={styles.label_and_input_price_box}>
+                  <p>Стоимость*</p>
+                  <input></input>
+                </div>
+                <div className={styles.label_and_input_box}>
+                  <p>Условия*</p>
+                  <input></input>
+                </div>
+              </div>
+            </div>
+            <div className={styles.send_form_box}>
+              <p>Отправить заявку</p>
+              <Link href="">
+                <Image
+                  src = "/student/imgs/send_lot_form_button.svg"
+                  width = {58}
+                  height = {56}
+                  alt = ""
+                />
+              </Link>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </main>
+  )
+}
--- frontend/src/components/LotCard/lot-card.tsx
+++ frontend/src/components/LotCard/lot-card.tsx
@@ -1,4 +1,6 @@
 import styles from './card.module.css';
+import { Mulish, Jura } from 'next/font/google'
+
 interface TradeLotCardProps {
     number: number;
     description: string;
@@ -6,6 +8,10 @@ interface TradeLotCardProps {
     price: number;
     conditions: string;
 }
+
+const jura = Jura({
+    subsets: ['latin']
+})
 
 const TradeLotCard: React.FC<TradeLotCardProps> = ({
     number,
@@ -15,24 +21,19 @@ const TradeLotCard: React.FC<TradeLotCardProps> = ({
     conditions,
 }) => {
     return (
-        <div className={styles.card}>
-            <div className={styles.header}>
-                <h3 className={styles.number}>Лот №{number}</h3>
+        <div className={styles.lot}>
+            <div className={jura.className}>
+                <h1 className={styles.lot_name}>Лот №{number}</h1>
             </div>
-            <div className={styles.body}>
-                <p className={styles.description}>Описание: {description}</p>
-                <p className={styles.performer}>
-                    <h1 className={styles.pink}> Исполнитель лота: </h1>
-                    {performer}
-                </p>
-                <p className={styles.price}>
-                    Стоимость лота: <span className={styles.bigBlue}>{price}</span>
-                </p>
-                <p className={styles.conditions}><span className={styles.smallBlue} >Условия:</span> {conditions}</p>
+            <p className={styles.lot_content}>{description}</p>
+            <p className={styles.lot_holder}>Исполнитель лота:</p>
+            <p className={styles.lot_holder_name}>{performer}</p>
+            <div className={styles.lot_price_container}>
+                <p className={styles.lot_price}>Стоимость (ШПрот):</p>
+                <p className={styles.lot_price_count}>{price}</p>
             </div>
-            <div className={styles.footer}>
-                <button className={styles.button}>Подать заявку</button>
-            </div>
+            <p className={styles.lot_condition_value}><span className={styles.lot_condition}>Условия:</span> {conditions}</p>
+            <button className={styles.application_lot_button}>Подать заявку</button>
         </div>
     );
 };
