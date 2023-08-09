ade60c275480d66d61997f061d20bf25f254efe0
3uo2khte66h <andrey_2468@mail.ru>
2023-08-09 04:09:52 +0300

    button fixes

--- frontend/src/components/Buttons/ExitButton/ExitButton.tsx
+++ frontend/src/components/Buttons/ExitButton/ExitButton.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import styles from './exitButton.module.css';
 import Image from 'next/image';
 import icon from '../../../../public/admin/log-out.png';
+import Link from 'next/link';
 
 export default function ExitButton() {
   return (
-    <button className={styles.button}>
+    <Link href={''} className={styles.button}>
       <p>Выйти</p>
-      <Image width={18} height={18} src={icon} alt={''}></Image>
-    </button>
+      <Image width={16} height={16} src={icon} alt={''}></Image>
+    </Link>
   );
 }
