31d2dc14d755b61ad9dcaf5e969cb55ae9df0709
3uo2khte66h <andrey_2468@mail.ru>
2023-08-12 20:55:56 +0300

    grade/edit page fix

--- frontend/src/app/admin/assessment/edit/[learnerId]/[taskId]/page.tsx
+++ frontend/src/app/admin/assessment/edit/[learnerId]/[taskId]/page.tsx
@@ -1,7 +1,12 @@
 'use client';
-import style from './page.module.css';
+import styles from './page.module.css';
 import React, { useState } from 'react';
-import {Input} from 'antd';
+import { Input } from 'antd';
+import { Jura } from 'next/font/google';
+
+const jura = Jura({
+  subsets: ['cyrillic'],
+});
 
 export default function Page() {
   const [intValue, setIntValue] = useState<number | ''>();
@@ -21,34 +26,55 @@ export default function Page() {
 
   return (
     <div>
-      <h1 className={style.title}>Редактирование оценки</h1>
-      <h1 className={style.big}>Тип</h1>
-      <h1 className={style.small}>Оц. в ведомость</h1>
+      <h1 className={`${styles.title} ${jura.className}`}>
+        РЕДАКТИРОВАНИЕ ОЦЕНКИ
+      </h1>
 
-      <h1 className={style.big}>Ученик</h1>
-      <h1 className={style.small}>Иванов Иван Иванович</h1>
-      <h1 className={style.big}>Задание</h1>
-      <h1 className={style.small}>ДЗ_1</h1>
+      <div className={styles.grade_props}>
+        <div className={styles.grade_props_component}>
+          <p className={styles.prop_type}>Тип</p>
+          <p className={styles.prop_value}>Оц. в ведомость</p>
+        </div>
+        <div className={styles.grade_props_component}>
+          <p className={styles.prop_type}>Ученик</p>
+          <p className={styles.prop_value}>Иванов Иван Иванович</p>
+        </div>
+        <div className={styles.grade_props_component}>
+          <p className={styles.prop_type}>Задание</p>
+          <p className={styles.prop_value}>ДЗ_1</p>
+        </div>
+      </div>
 
-      <h1 className={style.big}>Оценка</h1>
-      <Input
-        style={{ width: '7rem', height: '4rem', marginTop: '1rem' }}
-        type="number"
-        min={1}
-        max={10}
-        value={intValue}
-        onChange={handleNumberChange}
-      />
-      <h1 className={style.big}>Комментарий</h1>
-      <Input.TextArea
-        style={{ width: '14rem', marginTop: '1rem' }}
-        value={textValue}
-        rows={6}
-        margintTop={'10rem'}
-        onChange={handleTextChange}
-      />
-      <div className={style.pad}>
-        <button className={style.confirmButton}>Подтвердить изменения</button>
+      <div className={styles.input_box}>
+        <div className={styles.prop_label_container}>
+          <p className={styles.prop_label}>Оценка</p>
+          <Input
+            style={{
+              width: '4.5rem',
+              height: '2.5rem',
+              fontSize: '1.2rem',
+            }}
+            type="number"
+            min={1}
+            max={10}
+            value={intValue}
+            onChange={handleNumberChange}
+          />
+        </div>
+
+        <div className={styles.prop_label_container}>
+          <p className={styles.prop_label}>Комментарий</p>
+          <Input.TextArea
+            style={{ width: '14rem', fontSize: '1rem' }}
+            value={textValue}
+            rows={6}
+            onChange={handleTextChange}
+          />
+        </div>
+      </div>
+
+      <div className={styles.pad}>
+        <button className={styles.confirmButton}>Подтвердить изменения</button>
       </div>
     </div>
   );
