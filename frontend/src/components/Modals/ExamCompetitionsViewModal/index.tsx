0cc1a187cd7d86a5f47ddebedd3a773a27be3007
Nikita <n.d.2002z@gmail.com>
2024-05-05 19:16:26 +0300

    sm

--- frontend/src/components/Modals/ExamCompetitionsViewModal/index.tsx
+++ frontend/src/components/Modals/ExamCompetitionsViewModal/index.tsx
@@ -1,28 +1,8 @@
-import { skipToken } from '@reduxjs/toolkit/query';
-import { Button, Modal } from 'antd';
+import { Modal } from 'antd';
 import cn from 'classnames/bind';
-import React, { MouseEventHandler } from 'react';
+import React, { MouseEventHandler, useEffect } from 'react';
 
-import PriceQuestionTooltip from '@/components/LotCard/components/QuestionTooltip';
-
-import styles from '../../Components/Styles/rootModalStyles.module.css';
-
-import { CheckOutlined, CloseOutlined, EditOutlined } from '@ant-design/icons';
-import Link from 'next/link';
-import dateToFormatString from '@/util/dateToFormatString';
-import {
-  useApproveRejectClaimMutation,
-  useGetFailedDeadlineClaimByIdQuery,
-} from '@/redux/services/api';
-import { ClaimStatus, TwoSidedClaimStatus } from '@/types/common';
-import {
-  claimStatusToString,
-  twoSidedClaimStatusToString,
-} from '@/util/enumsToString';
-import { ModalProperty } from '@/components/Modals/Components/ModalProperty';
-import { ModalButtonsGroup } from '@/components/Modals/Components/ModalButtonsGroup';
 import { ModalContainer } from '@/components/Modals/Components/ModalContainer';
-import { ModalSectionTitle } from '@/components/Modals/Components/ModalSectionTitle';
 import { ExamPage } from '@/components/Pages/ExamPage';
 import { CompetitionPage } from '@/components/Pages/CompetitionPage';
 
@@ -37,8 +17,6 @@ type Props = {
   isDeclineLoading?: boolean;
 };
 
-const cx = cn.bind(styles);
-
 export function ExamCompetitionsViewModal({
   examId,
   competitionId,
@@ -49,6 +27,9 @@ export function ExamCompetitionsViewModal({
   isOkLoading,
   isDeclineLoading,
 }: Props) {
+  useEffect(() => {
+    console.log('EX: ', examId, ' COMP: ', competitionId);
+  }, [examId, competitionId]);
 
   return (
     <Modal
@@ -59,8 +40,8 @@ export function ExamCompetitionsViewModal({
       centered
     >
       <ModalContainer>
-       {examId && <ExamPage examId={examId} />}
-       {competitionId && <CompetitionPage competitionId={competitionId} />}
+        {examId && <ExamPage examId={examId} />}
+        {competitionId && <CompetitionPage competitionId={competitionId} />}
       </ModalContainer>
     </Modal>
   );
